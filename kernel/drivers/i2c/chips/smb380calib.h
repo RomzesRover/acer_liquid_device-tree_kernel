ad867b5c26cda8e177a87618861a5b8f69cbbb9d
RomzesRover <RomzesRover@gmail.com>
2013-09-13 21:02:15 +0600

    use native beta4 sensor drivers

--- kernel/drivers/i2c/chips/smb380calib.h
+++ kernel/drivers/i2c/chips/smb380calib.h
@@ -1,72 +1,77 @@
-/*  $Date: 2009/03/24 18:33:20 $
- *  $Revision: 1.1 $ 
+/*  $Date: 2007/08/07 16:06:00 $
+ *  $Revision: 1.1 $
  */
 
+
 /*
- * Copyright (C) 2009 Bosch Sensortec GmbH
- *
- *	BMA150 / SMB380 In Line Calibration
- * 
- * Usage:	calibration of SMB380/BMA150 acceleration sensor
- *
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in 
-  compliance with the License and the following stipulations. The Apache License , Version 2.0 is applicable unless 
-  otherwise stated by the stipulations of the disclaimer below. 
- 
- * You may obtain a copy of the License at 
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-
-Disclaimer
- *
- * Common:
- * This Work is developed for the consumer goods industry. It may only be used 
- * within the parameters of the respective valid product data sheet.  The Work 
- * provided with the express understanding that there is no warranty of fitness for a particular purpose. 
- * It is not fit for use in life-sustaining, safety or security sensitive systems or any system or device 
- * that may lead to bodily harm or property damage if the system or device malfunctions. In addition, 
- * the Work is not fit for use in products which interact with motor vehicle systems.  
- * The resale and/or use of the Work are at the purchaser’s own risk and his own responsibility. The 
- * examination of fitness for the intended use is the sole responsibility of the Purchaser. 
- *
- * The purchaser shall indemnify Bosch Sensortec from all third party claims, including any claims for 
- * incidental, or consequential damages, arising from any Work or Derivative Work use not covered by the parameters of 
- * the respective valid product data sheet or not approved by Bosch Sensortec and reimburse Bosch 
- * Sensortec for all costs in connection with such claims.
- *
- * The purchaser must monitor the market for the purchased Work and Derivative Works, particularly with regard to 
- * product safety and inform Bosch Sensortec without delay of all security relevant incidents.
- *
- * Engineering Samples are marked with an asterisk (*) or (e). Samples may vary from the valid 
- * technical specifications of the product series. They are therefore not intended or fit for resale to third 
- * parties or for use in end products. Their sole purpose is internal client testing. The testing of an 
- * engineering sample may in no way replace the testing of a product series. Bosch Sensortec 
- * assumes no liability for the use of engineering samples. By accepting the engineering samples, the 
- * Purchaser agrees to indemnify Bosch Sensortec from all claims arising from the use of engineering 
- * samples.
- *
- * Special:
- * This Work and any related information (hereinafter called "Information") is provided free of charge 
- * for the sole purpose to support your application work. The Woek and Information is subject to the 
- * following terms and conditions: 
- *
- * The Work is specifically designed for the exclusive use for Bosch Sensortec products by 
- * personnel who have special experience and training. Do not use this Work or Derivative Works if you do not have the 
- * proper experience or training. Do not use this Work or Derivative Works fot other products than Bosch Sensortec products.  
- *
- * The Information provided is believed to be accurate and reliable. Bosch Sensortec assumes no 
- * responsibility for the consequences of use of such Information nor for any infringement of patents or 
- * other rights of third parties which may result from its use. No license is granted by implication or 
- * otherwise under any patent or patent rights of Bosch. Specifications mentioned in the Information are 
- * subject to change without notice.
+* Copyright (C) 2007 Bosch Sensortec GmbH
+*
+* SMB380 calibration routine based on SMB380 API
+*
+* Usage: calibration of SMB380/BMA150 acceleration sensor
+*
+* Author:	Lars.Beseke@bosch-sensortec.com
+*
+* Disclaimer
+*
+* Common:
+* Bosch Sensortec products are developed for the consumer goods industry. They may only be used
+* within the parameters of the respective valid product data sheet.  Bosch Sensortec products are
+* provided with the express understanding that there is no warranty of fitness for a particular purpose.
+* They are not fit for use in life-sustaining, safety or security sensitive systems or any system or device
+* that may lead to bodily harm or property damage if the system or device malfunctions. In addition,
+* Bosch Sensortec products are not fit for use in products which interact with motor vehicle systems.
+* The resale and/or use of products are at the purchaser?s own risk and his own responsibility. The
+* examination of fitness for the intended use is the sole responsibility of the Purchaser.
+*
+* The purchaser shall indemnify Bosch Sensortec from all third party claims, including any claims for
+* incidental, or consequential damages, arising from any product use not covered by the parameters of
+* the respective valid product data sheet or not approved by Bosch Sensortec and reimburse Bosch
+* Sensortec for all costs in connection with such claims.
+*
+* The purchaser must monitor the market for the purchased products, particularly with regard to
+* product safety and inform Bosch Sensortec without delay of all security relevant incidents.
+*
+* Engineering Samples are marked with an asterisk (*) or (e). Samples may vary from the valid
+* technical specifications of the product series. They are therefore not intended or fit for resale to third
+* parties or for use in end products. Their sole purpose is internal client testing. The testing of an
+* engineering sample may in no way replace the testing of a product series. Bosch Sensortec
+* assumes no liability for the use of engineering samples. By accepting the engineering samples, the
+* Purchaser agrees to indemnify Bosch Sensortec from all claims arising from the use of engineering
+* samples.
+*
+* Special:
+* This software module (hereinafter called "Software") and any information on application-sheets
+* (hereinafter called "Information") is provided free of charge for the sole purpose to support your
+* application work. The Software and Information is subject to the following terms and conditions:
+*
+* The Software is specifically designed for the exclusive use for Bosch Sensortec products by
+* personnel who have special experience and training. Do not use this Software if you do not have the
+* proper experience or training.
+*
+* This Software package is provided `` as is `` and without any expressed or implied warranties,
+* including without limitation, the implied warranties of merchantability and fitness for a particular
+* purpose.
+*
+* Bosch Sensortec and their representatives and agents deny any liability for the functional impairment
+* of this Software in terms of fitness, performance and safety. Bosch Sensortec and their
+* representatives and agents shall not be liable for any direct or indirect damages or injury, except as
+* otherwise stipulated in mandatory applicable law.
+*
+* The Information provided is believed to be accurate and reliable. Bosch Sensortec assumes no
+* responsibility for the consequences of use of such Information nor for any infringement of patents or
+* other rights of third parties which may result from its use. No license is granted by implication or
+* otherwise under any patent or patent rights of Bosch. Specifications mentioned in the Information are
+* subject to change without notice.
+*
+* It is not allowed to deliver the source code of the Software to any third party without permission of
+* Bosch Sensortec.
 */
 
 
 /*! \file smb380calib.h
     \brief This file contains all function headers for the SMB380/BMA150 calibration process
-        
+
 */
 
 
@@ -106,16 +111,16 @@ int smb380_verify_min_max(smb380acc_t , smb380acc_t , smb380acc_t );
 /** smb380_calibration routine
   \param orientation pass orientation one axis needs to be absolute 1 the others need to be 0
   \param tries number of iterative passes
-  \param *min, *max, *avg returns minimum, maximum and average offset 
+  \param *min, *max, *avg returns minimum, maximum and average offset
  */
 int smb380_calc_new_offset(smb380acc_t, smb380acc_t, unsigned short *, unsigned short *, unsigned short *);
 
-/** overall calibration process. This function takes care about all other functions 
+/** overall calibration process. This function takes care about all other functions
   \param orientation input for orientation [0;0;1] for measuring the device in horizontal surface up
   \param *tries takes the number of wanted iteration steps, this pointer returns the number of calculated steps after this routine has finished
-  \return 1: calibration passed 2: did not pass within N steps 
+  \return 1: calibration passed 2: did not pass within N steps
 */
 int smb380_store_calibration(unsigned short, unsigned short, unsigned short);
 
 
-#endif // endif __SMB380_CALIBRATION__
+#endif /* endif __SMB380_CALIBRATION__	*/
